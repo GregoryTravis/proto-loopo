48f473d72dadf47c86337f2541e7c49189784dc6
Gregory Travis <greg.m.travis@gmail.com>
2025-01-09 10:32:11 -0500

    building and recognized in logic, had to remove MPEZoneLayout equality operators as they were conflicting with ones that have been added to JUCE

--- Source/SamplerPluginDemo.h
+++ Source/SamplerPluginDemo.h
@@ -772,6 +772,7 @@ private:
 namespace juce
 {
 
+/*
 bool operator== (const MPEZoneLayout& a, const MPEZoneLayout& b)
 {
     if (a.getLowerZone() != b.getLowerZone())
@@ -787,6 +788,7 @@ bool operator!= (const MPEZoneLayout& a, const MPEZoneLayout& b)
 {
     return ! (a == b);
 }
+*/
 
 template<>
 struct VariantConverter<LoopMode>
