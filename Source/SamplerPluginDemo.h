90993b73e0f3a7416ee943bdd000169381cf2007
Gregory Travis <greg.m.travis@gmail.com>
2025-01-19 00:09:15 -0500

    shew enable flag

--- Source/SamplerPluginDemo.h
+++ Source/SamplerPluginDemo.h
@@ -72,10 +72,16 @@
 /* } */
 
 #define LOGFILE "/tmp/loopo.log"
+const bool enableShew = true;
+FileLogger *shew_fl = nullptr;
 
 void shew(const String &s) {
-  /* FileLogger fl(File(LOGFILE), "heyo"); */
-  /* fl.logMessage(s); */
+  if (enableShew) {
+    if (shew_fl == nullptr) {
+      shew_fl = new FileLogger(File(LOGFILE), "heyo");
+    }
+    shew_fl->logMessage(s);
+  }
 }
 
 template <typename ValueType>
@@ -133,6 +139,7 @@ std::vector<AudioBuffer<float>*> *readLoopDir(const String dirname) {
 
     for (DirectoryEntry entry : RangedDirectoryIterator (File(dirname), false)) {
       /* juce::Logger::getCurrentLogger()->writeToLog("reading " + entry.getFile().getFullPathName()); */
+      shew("reading " + entry.getFile().getFullPathName());
       auto loop = readLoop(entry.getFile());
       if (loop != nullptr) {
         abs->push_back(loop);
